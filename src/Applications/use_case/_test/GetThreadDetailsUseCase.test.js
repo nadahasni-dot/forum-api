f380d4da21aa95bc879bca193cd4ef65f736a1db
Nada Hasni <nada.hasni@edi-indonesia.co.id>
2025-05-15 18:55:25 +0700

    fix: GetThreadDetailsUseCase.test.js

--- src/Applications/use_case/_test/GetThreadDetailsUseCase.test.js
+++ src/Applications/use_case/_test/GetThreadDetailsUseCase.test.js
@@ -62,23 +62,49 @@ describe('GetThreadDetailsUseCase', () => {
     const thread = await getThreadDetailsUseCase.execute(useCasePayload);
 
     // Assert
-    expect(thread.id).toEqual('thread-123');
-    expect(thread.title).toEqual('Thread baru');
-    expect(thread.body).toEqual('lorem ipsum dolor sit amet');
-    expect(thread.date).toEqual(mockTime);
-    expect(thread.username).toEqual('superman');
+    expect(thread).toEqual({
+      id: 'thread-123',
+      title: 'Thread baru',
+      body: 'lorem ipsum dolor sit amet',
+      date: mockTime,
+      username: 'superman',
+      comments: [
+        {
+          id: 'comment-1',
+          username: 'batman',
+          date: mockTime,
+          content: 'Komentar 1',
+        },
+        {
+          id: 'comment-2',
+          username: 'spiderman',
+          date: mockTime,
+          content: 'Komentar 2',
+        },
+        {
+          id: 'comment-3',
+          username: 'wonderwoman',
+          date: mockTime,
+          content: '**komentar telah dihapus**',
+        },
+      ],
+    });
 
     expect(thread.comments).toHaveLength(3);
 
-    expect(thread.comments[0].id).toEqual('comment-1');
-    expect(thread.comments[0].username).toEqual('batman');
-    expect(thread.comments[0].date).toEqual(mockTime);
-    expect(thread.comments[0].content).toEqual('Komentar 1');
+    expect(thread.comments[0]).toEqual({
+      id: 'comment-1',
+      username: 'batman',
+      date: mockTime,
+      content: 'Komentar 1',
+    });
 
-    expect(thread.comments[2].id).toEqual('comment-3');
-    expect(thread.comments[2].username).toEqual('wonderwoman');
-    expect(thread.comments[2].date).toEqual(mockTime);
-    expect(thread.comments[2].content).toEqual('**komentar telah dihapus**');
+    expect(thread.comments[2]).toEqual({
+      id: 'comment-3',
+      username: 'wonderwoman',
+      date: mockTime,
+      content: '**komentar telah dihapus**',
+    });
 
     expect(mockThreadRepository.getThreadById).toBeCalledWith(useCasePayload.threadId);
     expect(mockCommentRepository.getCommentsByThreadId).toBeCalledWith(useCasePayload.threadId);
